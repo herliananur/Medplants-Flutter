f15e3970c3f401e1c945e44cbb2ce0d97cb87797
herliananur <herliananur19@gmail.com>
2023-10-25 02:24:13 +0800

    menambahkan splashscreen

---
+++ medplants/lib/screen/splash_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:medplants/screen/home_page.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _navigatetohome();
+  }
+
+  _navigatetohome() async {
+    await Future.delayed(const Duration(seconds: 3), () {});
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const HomePage(),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0XFF27963C),
+      body: Center(
+          child: Image.asset(
+        'assets/logo.png',
+        width: 350,
+        height: 350,
+      )),
+    );
+  }
+}
